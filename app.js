8b7b3c4023341159c4c9eed834032405f60805ca
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2021-11-16 16:26:21 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const http = require("http");
 
 const app = express();
-
-app.set("port", 8000);
 
 // app.use를 사용해서 데이터를 받아옴
 // app.use(function (req, res) {
@@ -12,6 +9,6 @@ app.set("port", 8000);
 
 app.use(express.static(__dirname + "/workspace"));
 
-http.createServer(app).listen(app.get("port"), function () {
-    console.log(`express server on : ${app.get("port")}`);
+app.listen(8080, function () {
+    console.log(`express server on : 8080`);
 });
