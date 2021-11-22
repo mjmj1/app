40b1ec1ac9ff01afed9a88f5acf18b556dd56591
mjmj1 <80734723+mjmj1@users.noreply.github.com>
2021-11-22 21:44:11 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,63 @@
 const express = require("express");
-
+const multer = require("multer");
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
+// const upload = multer({ dest: "uploads/" });
 const app = express();
 
-// app.use를 사용해서 데이터를 받아옴
-// app.use(function (req, res) {
+const diretory = fs.existsSync("./workspace/images");
+if (!diretory) fs.mkdirSync("./workspace/images");
 
-// });
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, res, cb) {
+            cb(null, "./workspace/images");
+        },
+        filename: function (req, file, cb) {
+            let ext = path.extname(file.originalname);
+            let newName = Date.now();
 
-app.use(express.static(__dirname + "/workspace"));
+            cb(null, newName + ext);
+        },
+    }),
+    limits: {
+        files: 5,
+    },
+});
 
 app.listen(8080, function () {
-    console.log(`express server on : 8080`);
+    console.log("express server on : 8080");
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/workspace"));
+app.use(express.static(__dirname + "/workspace/images"));
+
+app.post("/upload-multiple", upload.array("imgUp", 5), (req, res) => {
+    let urlArr = new Array();
+
+    for (let i = 0; i < req.files.length; i++) {
+        urlArr.push(`/workspace/images/${req.files[i].filename}`);
+
+        try {
+            sharp(req.files[i].path)
+                .resize({ width: 400, height: 400 })
+                .withMetadata()
+                .toBuffer((e, buffer) => {
+                    if (e) throw err;
+                    fs.writeFile(req.files[i].path, buffer, (e) => {
+                        if (e) throw e;
+                    });
+                });
+        } catch (e) {
+            console.log(e);
+        }
+
+        app.get(`/img${i + 1}`, (req, res) => {
+            res.sendFile(__dirname + urlArr[i]);
+            console.log(__dirname + urlArr[i]);
+        });
+    }
+    res.redirect("/");
 });
